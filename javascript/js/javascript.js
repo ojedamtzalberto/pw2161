db2a0d737386f0943e7149aea94fb5454056c362
ojedamtzalberto <ojedamtz.alberto@gmail.com>
2016-04-12 10:59:10 -0600

    Archivo JS, funcion anonima y consola

---
+++ javascript/js/javascript.js
@@ -0,0 +1,8 @@
+var botonClic = function() {
+	alert("Clic del botón 2");
+}
+
+var nombre = "Alberto";
+document.write("<strong>JS en acción</strong>"+" "+nombre);
+console.log("A punto de lanzar un mensaje");
+alert("Mensaje por pantalla");
