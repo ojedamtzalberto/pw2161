6c3e5c1634c7bfc18d5f07dbb37354f117d96f6a
ojedamtzalberto <ojedamtz.alberto@gmail.com>
2016-04-14 10:53:22 -0600

    Calculadora terminada

---
+++ calculadora/js/calculadora.js
@@ -0,0 +1,39 @@
+//Variable global
+var operador = "";
+
+function igual() {
+	var valor1 = document.calculadora.operando1.value;	
+	var valor2 = document.calculadora.operando2.value;	
+
+	document.calculadora.resultado.value = eval(valor1+operador+valor2);
+}
+
+function limpiar() {
+	document.calculadora.operando1.value = "0";
+	document.calculadora.operando2.value = "0";
+	document.calculadora.resultado.value = "0";
+	operador = "";
+}
+
+function numeros(num) {
+	var valor = document.calculadora.operando1.value;
+	if(operador == "") { //Escribir en el operando1
+		if(valor== "0") 
+			document.calculadora.operando1.value = "";
+		
+		document.calculadora.operando1.value = 
+		document.calculadora.operando1.value + num;
+	}
+	else { //Escribir en el operando2
+		var valor = document.calculadora.operando2.value;
+		if(valor== "0") 
+			document.calculadora.operando2.value = "";
+		
+		document.calculadora.operando2.value = 
+		document.calculadora.operando2.value + num;
+	}
+}
+
+function operadores(ope) {
+	operador = ope;
+}
